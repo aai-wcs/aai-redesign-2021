6ad7d98aef99566d293f62441cc490e6f0974a19
Edward Margallo <edward.margallo@automationanywhere.com>
2022-01-13 14:05:34 -0800

    New AAI redesign html/css added
    New AAI redesign html/css added showcasing buttons styles and micro animations with interactivity (hover/active/focus). Also testing out possible new font pairings.

--- public/js/main.polyfills.js
+++ public/js/main.polyfills.js
@@ -1,4 +1,4 @@
-/*! AAI-WCS Web Redesign v0.0.1 | (c) 2021 Edward Margallo | MIT License | https://github.com/aai-wcs/aai-redesign-2021 */
+/*! AAI-WCS Web Redesign v0.0.1 | (c) 2022 Edward Margallo | MIT License | https://github.com/aai-wcs/aai-redesign-2021 */
 document.addEventListener('click', (function (event) {
 	if (!event.target.matches('#click-me')) return;
 	alert('You clicked me!');
