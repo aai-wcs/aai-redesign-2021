0f01397d007bfef9a330e1f73d71c42a886abcbe
edward.margallo <emargallo@matrixwebs.com>
2022-06-15 21:26:30 -0700

    README Update
    Clean up copy

--- public/js/main.polyfills.js
+++ public/js/main.polyfills.js
@@ -1,8 +1,4 @@
 /*! AAI-WCS Web Redesign v0.0.1 | (c) 2022 Edward Margallo | MIT License | https://github.com/aai-wcs/aai-redesign-2021 */
-document.addEventListener('click', (function (event) {
-	if (!event.target.matches('#click-me')) return;
-	alert('You clicked me!');
-}), false);
 /**
  * Element.matches() polyfill (simple version)
  * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
@@ -10,3 +6,7 @@ document.addEventListener('click', (function (event) {
 if (!Element.prototype.matches) {
 	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
 }
+document.addEventListener('click', (function (event) {
+	if (!event.target.matches('#click-me')) return;
+	alert('You clicked me!');
+}), false);
