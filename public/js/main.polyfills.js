da583cff13b9c0a6ed481c520cc27457dc7c52ec
Edward Margallo <edward.margallo@automationanywhere.com>
2022-07-06 14:49:25 -0700

    Major Content Updates
    Per Gabe's PPT slide deck. Still missing some content for functions.

--- public/js/main.polyfills.js
+++ public/js/main.polyfills.js
@@ -1,11 +1,4 @@
 /*! AAI-WCS Web Redesign v0.0.1 | (c) 2022 Edward Margallo | MIT License | https://github.com/aai-wcs/aai-redesign-2021 */
-/**
- * Element.matches() polyfill (simple version)
- * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
- */
-if (!Element.prototype.matches) {
-	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
-}
 document.addEventListener('click', (function (event) {
 	// if (!event.target.matches('#platform-discover')) return;
 	// alert('You clicked me!');
@@ -20,3 +13,10 @@ anchor.scrollIntoView();
 
 // TO DO
 // Vanilla JS - css style switcher, light mode vs dark mode
+/**
+ * Element.matches() polyfill (simple version)
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
+ */
+if (!Element.prototype.matches) {
+	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
