edac5f53e7305bcfe9ba2edd76e02d8d9b1a9b75
edward.margallo <emargallo@matrixwebs.com>
2023-03-20 12:10:27 -0700

    HP 2023 Q1 - Interactions and Animations
    Net new page, initial rough prototyping on home page with GSAP implementation

--- public/js/main.polyfills.js
+++ public/js/main.polyfills.js
@@ -1,4 +1,11 @@
-/*! AAI-WCS Web Redesign v0.0.1 | (c) 2022 Edward Margallo | MIT License | https://github.com/aai-wcs/aai-redesign-2021 */
+/*! AAI-WCS Web Redesign v0.0.1 | (c) 2023 Edward Margallo | MIT License | https://github.com/aai-wcs/aai-redesign-2021 */
+/**
+ * Element.matches() polyfill (simple version)
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
+ */
+if (!Element.prototype.matches) {
+	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
 document.addEventListener('click', (function (event) {
 	// if (!event.target.matches('#platform-discover')) return;
 	// alert('You clicked me!');
@@ -13,10 +20,3 @@ anchor.scrollIntoView();
 
 // TO DO
 // Vanilla JS - css style switcher, light mode vs dark mode
-/**
- * Element.matches() polyfill (simple version)
- * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
- */
-if (!Element.prototype.matches) {
-	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
-}
